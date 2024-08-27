2e907be5f269ff70666cd595bf9e1e985bc95103
Demonity11 <xxdm_11@outlook.com>
2024-08-27 09:35:38 -0300

    Pages posição, velocidade and espaço added. Initial css for these was made

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/posicao")
+def posicao():
+    return render_template("posicao.html")
+
+@app.route("/velocidade")
+def velocidade():
+    return render_template("velocidade.html")
+
+@app.route("/espaco")
+def espaco():
+    return render_template("espaco.html")
