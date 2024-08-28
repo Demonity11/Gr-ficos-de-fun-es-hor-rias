7d88e9fca94c0d50634b09cf5ed5faad475de3b4
Demonity11 <xxdm_11@outlook.com>
2024-08-28 10:22:33 -0300

    floating numbers fixed

--- app.py
+++ app.py
@@ -25,37 +25,39 @@ def result():
     t = request.form.get("t")
     if "." in t or "," in t:
         t = t.replace(",", ".") if "," in t else t
+        tempo_decimal = True
 
     t_arredondado = ceil(float(t))
     tempo = [str(_)+"s" for _ in range(0, t_arredondado)]
     tempo.append(t+"s")
+
+    s0 = request.form.get("s0")
+    if "." in s0 or "," in s0:
+        s0 = s0.replace(",", ".") if "," in s0 else s0
+
+    s0_arredondado = ceil(float(s0))
+    s0_ = s0_arredondado - float(s0)
+    
+    ac = request.form.get("ac")
+    if "." in ac or "," in ac:
+        ac = ac.replace(",", ".") if "," in ac else ac
+
+    ac_arredondado = ceil(float(ac))
+    ac_ = ac_arredondado - float(ac) 
 
     if request.form.get("ac") and request.form.get("s0") and request.form.get("v0"):
         s0 = request.form.get("s0")
         ac = request.form.get("ac")
 
     elif request.form.get("ac") and request.form.get("s0"):
-        s0 = request.form.get("s0")
-        if "." in s0 or "," in s0:
-            s0 = s0.replace(",", ".") if "," in s0 else s0
-
-        s0_arredondado = ceil(float(s0))
-        s0_ = s0_arredondado - float(s0)
-        print(s0_)
-        
-        ac = request.form.get("ac")
-        if "." in ac or "," in ac:
-            ac = ac.replace(",", ".") if "," in ac else ac
-
-        ac_arredondado = ceil(float(ac))
-        ac_ = ac_arredondado - float(ac) 
-        print(ac_)
-
         velocidade = [s0]
+        multiplicador = 1
         for _ in range(s0_arredondado, s0_arredondado + ac_arredondado*len(tempo), ac_arredondado):
             if _ > s0_arredondado:
-                velocidade.append(_ - s0_ - ac_)
+                velocidade.append(_ - (ac_*multiplicador + s0_))
+                multiplicador += 1
 
-        print(velocidade)
-    
+        if tempo_decimal:
+            velocidade[-1] = float(s0) + float(ac)*float(t)
+   
         return render_template("result.html", tempo=tempo, velocidade=velocidade)
