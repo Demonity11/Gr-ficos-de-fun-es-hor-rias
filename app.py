addefc850ec5c1c2027771f333422db5757c6011
Demonity11 <xxdm_11@outlook.com>
2024-08-28 09:37:17 -0300

    speed graphic added, but there's a problem with float numbers

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from math import ceil
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -18,3 +19,43 @@ def velocidade():
 @app.route("/espaco")
 def espaco():
     return render_template("espaco.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    t = request.form.get("t")
+    if "." in t or "," in t:
+        t = t.replace(",", ".") if "," in t else t
+
+    t_arredondado = ceil(float(t))
+    tempo = [str(_)+"s" for _ in range(0, t_arredondado)]
+    tempo.append(t+"s")
+
+    if request.form.get("ac") and request.form.get("s0") and request.form.get("v0"):
+        s0 = request.form.get("s0")
+        ac = request.form.get("ac")
+
+    elif request.form.get("ac") and request.form.get("s0"):
+        s0 = request.form.get("s0")
+        if "." in s0 or "," in s0:
+            s0 = s0.replace(",", ".") if "," in s0 else s0
+
+        s0_arredondado = ceil(float(s0))
+        s0_ = s0_arredondado - float(s0)
+        print(s0_)
+        
+        ac = request.form.get("ac")
+        if "." in ac or "," in ac:
+            ac = ac.replace(",", ".") if "," in ac else ac
+
+        ac_arredondado = ceil(float(ac))
+        ac_ = ac_arredondado - float(ac) 
+        print(ac_)
+
+        velocidade = [s0]
+        for _ in range(s0_arredondado, s0_arredondado + ac_arredondado*len(tempo), ac_arredondado):
+            if _ > s0_arredondado:
+                velocidade.append(_ - s0_ - ac_)
+
+        print(velocidade)
+    
+        return render_template("result.html", tempo=tempo, velocidade=velocidade)
